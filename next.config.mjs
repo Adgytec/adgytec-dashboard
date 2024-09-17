0f7f2811da5afeb12b42c5eaebf5a7b82dbf0337
rohan <rohanverma031@gmail.com>
2024-09-17 15:28:48 +0530

    fix: updated suspense bailout

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "blr1.digitaloceanspaces.com",
-			},
-		],
-	},
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "blr1.digitaloceanspaces.com",
+            },
+        ],
+    },
+    experimental: {missingSuspenseWithCSRBailout: false,},
 };
 
 export default nextConfig;
