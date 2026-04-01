e23dcf71a85c4b2ed97e82bbcede944da6b7a2dc
rohan <rohanverma031@gmail.com>
2026-04-01 16:59:54 +0530

    update: added change log

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -4,30 +4,33 @@ import React from "react";
 import styles from "./home.module.scss";
 import Container from "@/components/Container/Container";
 import Image from "next/image";
+import { Changelog } from "@/components/Changelog/Changelog";
 
 const Home = () => {
-	return (
-		<div className={styles.home}>
-			<Container className={styles.container}>
-				<h1> Welcome to the Adgytec Dashboard!👋</h1>
+    return (
+        <div className={styles.home}>
+            <Container className={styles.container}>
+                {/* <h1> Welcome to the Adgytec Dashboard!👋</h1> */}
 
-				<p>
-					Here you can manage all your project content. Simply select
-					an option from the navigation bar to get started.
-				</p>
+                {/* <p> */}
+                {/*     Here you can manage all your project content. Simply select */}
+                {/*     an option from the navigation bar to get started. */}
+                {/* </p> */}
 
-				<div className={styles.image}>
-					<Image
-						src="/home.png"
-						alt=""
-						width={420}
-						height={370}
-						priority
-					/>
-				</div>
-			</Container>
-		</div>
-	);
+                <Changelog />
+
+                <div className={styles.image}>
+                    <Image
+                        src="/home.png"
+                        alt=""
+                        width={420}
+                        height={370}
+                        priority
+                    />
+                </div>
+            </Container>
+        </div>
+    );
 };
 
 export default Home;
