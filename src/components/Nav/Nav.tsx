3a231e7fcb4f025f5123d75b501c95709653a1e6
rohan <rohanverma031@gmail.com>
2026-06-08 14:19:50 +0530

    feat: updated nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,19 @@
 import {
     NavigationLink as AriaNavigationLink,
     Navigation,
+    NavigationButton,
     NavigationSection,
     NavigationSectionLabel,
+    SubNavigation,
+    SubNavigationTrigger,
 } from "@adgytec/adgytec-web-ui-components";
 import {
     Folder,
     FolderPen,
+    FolderPlus,
     House,
     UserRoundPen,
+    UserRoundPlus,
     UsersRound,
 } from "lucide-react";
 import Link from "next/link";
@@ -50,8 +55,11 @@ export const Nav = () => {
             label={isNavigationDocked ? undefined : "Adgytec"}
             stateID="dashboard"
             isLinkActive={(href) => {
-                if (!href) return false;
-                return pathName.includes(href);
+                return pathName === href;
+            }}
+            isButtonActive={(prefix) => {
+                if (!prefix) return false;
+                return pathName.includes(prefix);
             }}
         >
             <NavigationSection>
@@ -77,17 +85,50 @@ export const Nav = () => {
                         Administration
                     </NavigationSectionLabel>
 
-                    <NavigationLink
-                        href="/admin/user"
-                        label="Users"
-                        icon={UsersRound}
-                    />
+                    <SubNavigationTrigger stateID="admin-users" label="Users">
+                        <NavigationButton
+                            prefix="/admin/user"
+                            icon={UsersRound}
+                        />
 
-                    <NavigationLink
-                        href="/admin/project"
+                        <SubNavigation>
+                            <NavigationLink
+                                href="/admin/user"
+                                label="Users"
+                                icon={UsersRound}
+                            />
+
+                            <NavigationLink
+                                href="/admin/user/create"
+                                label="Create User"
+                                icon={UserRoundPlus}
+                            />
+                        </SubNavigation>
+                    </SubNavigationTrigger>
+
+                    <SubNavigationTrigger
+                        stateID="admin-projects"
                         label="Projects"
-                        icon={FolderPen}
-                    />
+                    >
+                        <NavigationButton
+                            prefix="/admin/project"
+                            icon={FolderPen}
+                        />
+
+                        <SubNavigation>
+                            <NavigationLink
+                                href="/admin/project"
+                                label="Projects"
+                                icon={FolderPen}
+                            />
+
+                            <NavigationLink
+                                href="/admin/project/create"
+                                label="Create Project"
+                                icon={FolderPlus}
+                            />
+                        </SubNavigation>
+                    </SubNavigationTrigger>
                 </NavigationSection>
             )}
 
