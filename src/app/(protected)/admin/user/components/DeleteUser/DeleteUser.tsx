8e99f3099dc3999aa6e73e33f0959a5b0f11afec
rohan <rohanverma031@gmail.com>
2026-06-09 09:10:23 +0530

    feat: added delete user api

--- src/app/(protected)/admin/user/components/DeleteUser/DeleteUser.tsx
+++ src/app/(protected)/admin/user/components/DeleteUser/DeleteUser.tsx
@@ -4,9 +4,13 @@ import {
     Button,
     ModalOverlay,
     typography,
+    useSnackbarQueue,
 } from "@adgytec/adgytec-web-ui-components";
 import clsx from "clsx";
+import { useContext } from "react";
 import { Heading, Text } from "react-aria-components";
+import { useBoolean } from "usehooks-ts";
+import { UserContext } from "@/components/AuthContext/authContext";
 import { useUserAction } from "../Users";
 import styles from "./deleteUser.module.css";
 
@@ -15,30 +19,92 @@ export const DeleteUser: React.FC<{
     isOpen: boolean;
     onOpenChange: (val: boolean) => void;
 }> = ({ userID, isOpen, onOpenChange }) => {
+    const userWithRole = useContext(UserContext);
+    const currentUser = userWithRole ? userWithRole.user : null;
+
     const { onDeleteSuccess } = useUserAction();
+    const snackbarQueue = useSnackbarQueue();
+
+    const {
+        value: isDeleting,
+        setTrue: startDeleting,
+        setFalse: stopDeleting,
+    } = useBoolean();
+
+    const deleteUser = async (close: () => void) => {
+        startDeleting();
+
+        const url = `${process.env.NEXT_PUBLIC_API}/user/${userID}`;
+        const token = await currentUser?.getIdToken();
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        fetch(url, {
+            method: "DELETE",
+            headers,
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.error) throw new Error(res.message);
+
+                onDeleteSuccess(userID);
+                snackbarQueue.add(
+                    {
+                        supportingText: "User deleted successfully.",
+                    },
+                    {
+                        timeout: 5000,
+                    }
+                );
+            })
+            .catch((err) => {
+                snackbarQueue.add({ supportingText: err.message });
+            })
+            .finally(() => {
+                stopDeleting();
+                close();
+            });
+    };
 
     return (
-        <ModalOverlay isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalOverlay
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            isKeyboardDismissDisabled={isDeleting}
+        >
             <BottomSheetModal layout="detached">
                 <BottomSheet className={clsx(styles["dialog"])}>
-                    <Heading className={clsx(typography.titleLarge)}>
-                        Delete User?
-                    </Heading>
+                    {({ close }) => (
+                        <>
+                            <Heading className={clsx(typography.titleLarge)}>
+                                Delete User?
+                            </Heading>
 
-                    <Text className={clsx(typography.bodyMedium)}>
-                        This action cannot be undone. The user account will be
-                        permanently deleted.
-                    </Text>
+                            <Text className={clsx(typography.bodyMedium)}>
+                                This action cannot be undone. The user account
+                                will be permanently deleted.
+                            </Text>
 
-                    <div className={clsx(styles["actions"])}>
-                        <Button slot="close" color="text">
-                            Cancel
-                        </Button>
+                            <div className={clsx(styles["actions"])}>
+                                <Button
+                                    slot="close"
+                                    color="text"
+                                    isDisabled={isDeleting}
+                                >
+                                    Cancel
+                                </Button>
 
-                        <Button className={clsx(styles["delete-confirm"])}>
-                            Delete
-                        </Button>
-                    </div>
+                                <Button
+                                    isPending={isDeleting}
+                                    onPress={() => deleteUser(close)}
+                                    className={clsx(styles["delete-confirm"])}
+                                >
+                                    Delete
+                                </Button>
+                            </div>
+                        </>
+                    )}
                 </BottomSheet>
             </BottomSheetModal>
         </ModalOverlay>
